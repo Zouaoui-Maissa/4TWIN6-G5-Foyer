352367e79ef87273bb244ac1a1d83f1f3e5c3715
khalloufiHamzaUFO <hamza.khalloufi@esprit.tn>
2024-02-26 10:39:26 +0100

    MissingRepos

---
+++ src/main/java/tn/esprit/brogram/backend/DAO/Repositories/ChamberRepository.java
@@ -0,0 +1,27 @@
+package tn.esprit.brogram.backend.DAO.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tn.esprit.brogram.backend.DAO.Entities.Chamber;
+import tn.esprit.brogram.backend.DAO.Entities.TypeChamber;
+
+import java.util.List;
+
+public interface ChamberRepository extends JpaRepository<Chamber,Long> {
+  //  Chamber findByRes(Set<Reservation> r);
+    Chamber findChamberByResIdReservation(String id);
+
+  List<Chamber> findByBloc(Bloc b);
+
+  int countChamberByTypeCAndBloc_IdBloc(TypeChamber typeChamber , long idBloc);
+  List<Chamber> findChamberByBlocFoyerNomFoyerAndTypeCAndRes_Empty(String NomFoyer , TypeChamber type);
+
+
+  List<Chamber> findByTypeC(TypeChamber type);
+
+  List<Chamber> findByTypeCAndBlocNomBloc(TypeChamber type, String blocName);
+    List<Chamber> findChamberByBlocFoyerUniversiteNomUniversite(String nomUniversite);
+    Chamber findChamberByNumerochamber(long numero);
+
+    List<Chamber> findChamberByTypeCAndAndBlocFoyerUniversiteNomUniversite(TypeChamber typeChamber , String nomUniversite);
+
+}
