ac60c06a3f61156a0a4fb5466f66723dd0385d2d
hamza khalloufi <149695381+khalloufiHamzaUFO@users.noreply.github.com>
2024-02-26 11:07:20 +0100

    Update ChamberRepository.java

--- src/main/java/tn/esprit/brogram/backend/DAO/Repositories/ChamberRepository.java
+++ src/main/java/tn/esprit/brogram/backend/DAO/Repositories/ChamberRepository.java
@@ -1,10 +1,13 @@
 package tn.esprit.brogram.backend.DAO.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import tn.esprit.brogram.backend.DAO.Entities.Bloc;
 import tn.esprit.brogram.backend.DAO.Entities.Chamber;
+import tn.esprit.brogram.backend.DAO.Entities.Reservation;
 import tn.esprit.brogram.backend.DAO.Entities.TypeChamber;
 
 import java.util.List;
+import java.util.Set;
 
 public interface ChamberRepository extends JpaRepository<Chamber,Long> {
   //  Chamber findByRes(Set<Reservation> r);
